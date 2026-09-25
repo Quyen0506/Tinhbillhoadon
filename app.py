7bb3576e11eafa78bf361ee7a2beddb6deeb2585
Quyen0506 <24030053@student.bdu.edu.vn>
2026-09-25 10:06:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 import pandas as pd
 import streamlit as st
- 
+ st.image("Nhà Hàng Bé Sữa. PNG")
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
  
 # Đường dẫn file dữ liệu dùng chung trên máy chủ
